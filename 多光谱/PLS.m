3b16f86f3a885537ceeb8c8539379f29d97a51f0
ChangHL <changhuilin98@gmail.com>
2023-11-02 10:11:41 +0800

    hsi

--- 多光谱/PLS.m
+++ 多光谱/PLS.m
@@ -3,11 +3,11 @@
  plsrmse(Model,0);     %得出主成分数，0是个参数
  
  num_total =10;   %主成分数
- %训练
+ %使用整个光谱作为一个区间进行训练，得到一个模型
  plspvsm(Model,num_total,1);
  oneModel=plsmodel(Model,1,num_total,'mean','test',5);  
  %矫正
- predModel=plspredict(Xc,oneModel,num_total,Yc);%获得Rc，Yc2
+ predModel=plspredict(Xc,oneModel,num_total,Yc);%获得Rc，进入结构体predModel,获得Yc2
  plspvsm(predModel,num_total,1);    %把图标内的RMSEP改成RMSEC，这里函数默认输出显示RMSEP，实际上输出RMSEC，需要改一下图内的RMSEP为RMSEC
 %预测
  predModel=plspredict(Xt,oneModel,num_total,Yt);%获得Rp，Yt2
