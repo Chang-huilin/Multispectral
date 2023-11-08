34c540ff79a876eee47a9519c7f7535a7328b62a
ChangHL <changhuilin98@gmail.com>
2023-11-08 15:52:28 +0800

    预处理

--- 多光谱/PLS.m
+++ 多光谱/PLS.m
@@ -12,3 +12,39 @@
 %预测
  predModel=plspredict(Xt,oneModel,num_total,Yt);%获得Rp，Yt2
  plspvsm(predModel,num_total,1,1);      %预测集的结果  
+
+
+ 
+
+ % 建立PLS模型
+num_components = 10; % 定义PLS模型的主成分数目
+[XL,yl,XS,YS,beta,PCTVAR,MSE,stats] = plsregress(Xc, Yc, num_components);
+
+% 在训练集上进行预测
+Yc_predicted = [ones(size(Xc, 1), 1), Xc] * beta;
+
+% 在测试集上进行预测
+Xt_with_bias = [ones(size(Xt, 1), 1), Xt];
+Yt_predicted = Xt_with_bias * beta;
+
+% 计算训练集的相关系数（Rc）
+Rc = corr(Yc, Yc_predicted);
+
+% 计算测试集的相关系数（Rp）
+Rp = corr(Yt, Yt_predicted);
+
+% 计算训练集的均方根误差（RMSEC）
+RMSEC = sqrt(mean((Yc - Yc_predicted).^2));
+
+% 计算测试集的均方根误差（RMSEP）
+RMSEP = sqrt(mean((Yt - Yt_predicted).^2));
+
+% 计算RPD值
+RPD = std(Yc) / RMSEP;
+
+% 输出结果
+disp(['训练集的相关系数 Rc: ', num2str(Rc)]);
+disp(['测试集的相关系数 Rp: ', num2str(Rp)]);
+disp(['训练集的均方根误差 RMSEC: ', num2str(RMSEC)]);
+disp(['测试集的均方根误差 RMSEP: ', num2str(RMSEP)]);
+disp(['RPD值为: ', num2str(RPD)]);
