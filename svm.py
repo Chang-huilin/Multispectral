ee3fe6c2652c7d24f22d9a5bc25e47ea130cf357
ChangHL <changhuilin98@gmail.com>
2024-01-05 15:02:54 +0800

    1.5

--- svm.py
+++ svm.py
@@ -9,13 +9,14 @@ import matplotlib.pylab as plt
 
 
 df=pd.read_excel(r'C:\Users\79365\OneDrive\桌面\图像-叶绿素\叶绿素\原始数据\day1_140.xlsx')
-x=df.iloc[:,1:5]
-y=df['目标']
+x=pd.read_excel(r"C:\Users\79365\OneDrive\桌面\图像-叶绿素\叶绿素\x.xlsx")
+y=pd.read_excel(r"C:\Users\79365\OneDrive\桌面\图像-叶绿素\叶绿素\y.xlsx", usecols=[2])
+y = y.values.ravel()
 #标准化
 std=StandardScaler()
 x_std=std.fit_transform(x)
 #拆分训练集
-x_train, x_test, y_train, y_test= train_test_split(x_std,y,train_size=0.75)
+x_train, x_test, y_train, y_test= train_test_split(x_std,y,train_size=0.6)
 #svm建模
 svm_regression = SVR()
 svm_regression.fit(x_train,y_train)
